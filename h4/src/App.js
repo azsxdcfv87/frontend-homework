2a8334737d5f579631792e43b591a6949f70aab9
taco hsu <allen.hsu@swag.live>
2023-12-03 15:32:40 +0800

    clear up

--- h4/src/App.js
+++ h4/src/App.js
@@ -1,36 +1,9 @@
-// // src/App.js
-// import React, { useState } from 'react';
-// import Pagination from '@mui/material/Pagination';
-// import Stack from '@mui/material/Stack';
-
-// const App = () => {
-//   const [currentPage, setCurrentPage] = useState(1);
-
-//   const handleChange = (event, value) => {
-//     setCurrentPage(value);
-//   };
-
-//   return (
-//     <div>
-//       <h1>Content Goes Here</h1>
-//       <Stack spacing={2}>
-//         <Pagination count={10} page={currentPage} onChange={handleChange} />
-//       </Stack>
-//     </div>
-//   );
-// };
-
-// export default App;
-
-
-// app.js
 import React from 'react';
-import Paginator from './Paginator'; // 这里根据您的项目结构和文件路径进行调整
+import Paginator from './Paginator';
 
 const App = () => {
   const handlePageChange = (currentPageInfo) => {
     console.log('Current Page:', currentPageInfo);
-    // 这里可以添加处理当前页码变化的逻辑
   };
 
   return (
--- h4/src/Paginator.js
+++ h4/src/Paginator.js
@@ -1,4 +1,3 @@
-// Paginator.js
 import React, { useState } from 'react';
 import './Paginator.css';
 
