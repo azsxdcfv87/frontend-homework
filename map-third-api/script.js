4ee58ebd8efc06453e3d8bcd10a02ec224e46e4b
taco hsu <allen.hsu@swag.live>
2024-03-11 15:29:20 +0800

    modify file

--- map-third-api/script.js
+++ map-third-api/script.js
@@ -1,4 +1,4 @@
-L.mapquest.key = '6j4tBiIajJtfmdVipqIPPQu6TVxjNnxd';
+L.mapquest.key = 'YOUR-API-KEY-HERE';
 
 // 'map' refers to a <div> element with the ID map
 const map = L.mapquest.map('map', {
