84f07b25b1d5a5c1eb7d45f49294a302dfc43fa6
taco hsu <allen.hsu@swag.live>
2024-03-13 17:36:03 +0800

    modify file

--- IsPrime.js
+++ IsPrime.js
@@ -1,9 +1,18 @@
 function isPrime(num) {
-    for(let i = 2; i < num; i++)
-        if (num % i === 0) return false; 
-    return num > 1;
+  if (num <= 1) return false;
+  if (num <= 3) return true;
+  if (num % 2 === 0 || num % 3 === 0) return false;
+
+  const nSqrt = Math.floor(Math.sqrt(n));
+
+  for (let i = 5; i <= nSqrt; i += 6) {
+    // n % i: 6n + 5 -> 6(n + 1) - 1 -> 6n - 1, n % (i + 2): 6n + 1
+    if (num % i === 0 || num % (i + 2) === 0) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
-
-console.log(isPrime(2687)); // true
-console.log(isPrime(9087)); // false
+console.log(isPrime(11)); // false
