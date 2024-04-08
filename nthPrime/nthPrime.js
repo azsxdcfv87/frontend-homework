bb38598545c8d87635086c494f6d16bf3d4fb3fe
taco hsu <allen.hsu@swag.live>
2024-04-08 10:46:34 +0800

    add batchfuc file

---
+++ nthPrime/nthPrime.js
@@ -0,0 +1,53 @@
+function nthPrime(n) {
+  return new Promise((resolve) => {
+    let primes = [];
+    let num = 2;
+    let count = 0;
+
+    function checkNextNumber() {
+      if (count < n) {
+        if (isPrime(num)) {
+          primes.push(num);
+          count++;
+        }
+        num++;
+        setTimeout(checkNextNumber, 0);
+      } else {
+        resolve(primes[n - 1]);
+      }
+    }
+
+    checkNextNumber();
+  });
+}
+
+function isPrime(num) {
+  if (num <= 1) {
+    return false;
+  }
+  if (num <= 3) {
+    return true;
+  }
+  if (num % 2 === 0 || num % 3 === 0) {
+    return false;
+  }
+
+  const numSqrt = Math.floor(Math.sqrt(num));
+
+  for (let i = 5; i <= numSqrt; i += 6) {
+    if (num % i === 0 || num % (i + 2) === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function calculatePrime() {
+  const position = document.getElementById('primePosition').value;
+  nthPrime(position).then(prime => {
+    document.getElementById('result').textContent = 'The ' + position + 'th prime number is ' + prime;
+  });
+}
+
+// nthPrime(20).then(prime => console.log(prime)); // 71
